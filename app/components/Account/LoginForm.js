2fed25bdb6a97ffe7520305492955d9bbbdcc466
Juan Pablo Loureyro <juanpabloloureyro@gmail.com>
2020-05-13 20:02:52 -0300

    login terminado

---
+++ app/components/Account/LoginForm.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Input, Icon, Button } from "react-native-elements";
+import { isEmpty } from "lodash";
+import Loading from "../Loading";
+import { validateEmail } from "../../utils/validations";
+import { useNavigation } from "@react-navigation/native";
+import * as firebase from "firebase";
+
+export default function LoginForm(props) {
+  const { toastRef } = props;
+  const navigation = useNavigation();
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState(defaultFormValue());
+  const [loading, setLoading] = useState(false);
+
+  const onChange = (e, type) => {
+    setFormData({ ...formData, [type]: e.nativeEvent.text });
+  };
+
+  const onSubmit = () => {
+    if (isEmpty(formData.email || formData.password)) {
+      toastRef.current.show("Todos los campos son obligatorios.");
+    } else if (!validateEmail(formData.email)) {
+      toastRef.current.show("El email no es correcto.");
+    } else {
+      setLoading(true);
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(formData.email, formData.password)
+        .then(() => {
+          setLoading(false);
+          navigation.navigate("account");
+        })
+        .catch(() => {
+          setLoading(false);
+          toastRef.current.show("Error");
+        });
+    }
+  };
+
+  return (
+    <View style={styles.formContainer}>
+      <Input
+        placeholder="Correo electronico"
+        containerStyle={styles.inputForm}
+        onChange={(e) => onChange(e, "email")}
+        rightIcon={
+          <Icon
+            type="material-community"
+            name="at"
+            iconStyle={styles.iconRight}
+          />
+        }
+      />
+      <Input
+        placeholder="Contraseña"
+        containerStyle={styles.inputForm}
+        password={true}
+        secureTextEntry={showPassword ? false : true}
+        onChange={(e) => onChange(e, "password")}
+        rightIcon={
+          <Icon
+            type="material-community"
+            name={showPassword ? "eye-off-outline" : "eye-outline"}
+            iconStyle={styles.iconRight}
+            onPress={() => setShowPassword(!showPassword)}
+          />
+        }
+      />
+      <Button
+        title="Iniciar sesion"
+        containerStyle={styles.btnContainerLogin}
+        buttonStyle={styles.btnLogin}
+        onPress={onSubmit}
+      />
+      <Loading isVisible={loading} text="Iniciando sesión" />
+    </View>
+  );
+}
+
+const defaultFormValue = () => {
+  return {
+    email: "",
+    password: "",
+  };
+};
+
+const styles = StyleSheet.create({
+  formContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
+  },
+  inputForm: {
+    width: "100%",
+  },
+  btnContainerLogin: {
+    marginTop: 20,
+    width: "95%",
+  },
+  btnLogin: {
+    backgroundColor: "#00a680",
+  },
+  iconRight: {
+    color: "#c1c1c1",
+  },
+});
--- app/screens/Account/Login.js
+++ app/screens/Account/Login.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 import { View, Text, StyleSheet, ScrollView, Image } from "react-native";
 import { Divider } from "react-native-elements";
 import { useNavigation } from "@react-navigation/native";
+import LoginForm from "../../components/Account/LoginForm";
+import Toast from "react-native-easy-toast";
 
 export default function Login() {
+  const toastRef = useRef();
+
   return (
     <ScrollView>
       <Image
@@ -12,11 +16,12 @@ export default function Login() {
         style={styles.logo}
       />
       <View style={styles.viewContainer}>
-        <Text>Login</Text>
+        <LoginForm toastRef={toastRef} />
         <CreateAccount />
       </View>
       <Divider style={styles.divider} />
       <Text>Social Login</Text>
+      <Toast ref={toastRef} position="center" opacity={0.9} />
     </ScrollView>
   );
 }
